d546c2b43c2a4f5b5bf6ee97428df88674048cdf
Josselin Giet <giet@clipper.ens.fr>
2019-02-27 22:40:36 +0100

    On essaye ...

--- simplifier.ml
+++ simplifier.ml
@@ -5,8 +5,6 @@ let rec size = function
     | _ -> 1
 
 let rec simplify = function
-  | App (App (Compose _, Identity _), u)
-  | App (App (Compose _, u), Identity _) -> u, true
   | App (
         App (Compose (ok0, OkPair (OkArrow (okd, okb),_), _ ), Apply _),
         App (App (
@@ -14,6 +12,7 @@ let rec simplify = function
                   App (Curry _, h)), 
              g)) ->
     let id = Identity ok0 in
+    let _ = Printf.printf "On simplifie un [curry apply] -> [f]\n" in
     let fork = Fork (ok0, ok0, okd) in
     let compose = Compose (ok0, OkPair (ok0, okd), okb) in
     App (
@@ -21,10 +20,15 @@ let rec simplify = function
         App (App (fork, id), g)
         ), true
 
+  | App (App (Compose _, Identity _), u)
+  | App (App (Compose _, u), Identity _) -> 
+          let _ = Printf.printf "On simplifie un [id o f] -> [f]\n" in
+    u, true
   | App (u, v) -> 
     let simp_u, modify_u = simplify u in
     let simp_v, modify_v = simplify v in
     App (simp_u, simp_v), modify_u || modify_v
+
 
   | x -> x, false
 
