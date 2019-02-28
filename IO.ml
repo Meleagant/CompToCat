c04aedbb457632aa14d1c19e1cc3e95a76b6dede
giet <giet@clipper.ens.fr>
2019-03-01 00:35:55 +0100

    setup ok pour inférence de types

--- IO.ml
+++ IO.ml
@@ -1,6 +1,10 @@
-let parse_input_source_file source_filename = Lexing.(
+let parse_input_source_file source_filename = 
+Lexing.(
   let cin = open_in source_filename in
   let buf = Lexing.from_channel cin in
   buf.lex_curr_p <- { buf.lex_curr_p with  pos_fname = source_filename };
-  Parser.program Lexer.token buf
+  if Filename.extension source_filename = ".j0" then  
+    Parser0.program Lexer0.token buf |> Typeinfer.infer_type
+  else
+    Parser.program Lexer.token buf
 )
---
+++ typeinfer.ml
@@ -0,0 +1,6 @@
+module S = Source
+module S0 = Source0
+
+
+let infer_type (prog: S0.program_with_locations) : S.program_with_locations = 
+    assert false
